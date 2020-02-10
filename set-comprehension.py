e3fc77965c30a2c268d150ec423a3ce958242f01
SaptarshiMisra <saptarshi.misra@gmail.com>
2020-02-10 16:18:52 +0530

    modified variable name for set comprehension code

--- set-comprehension.py
+++ set-comprehension.py
@@ -1,7 +1,7 @@
 def set_comp():
     old_list = [0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9]
-    new_list = {i * i for i in old_list if i % 2 == 0}
-    return new_list
+    new_set = {i * i for i in old_list if i % 2 == 0}
+    return new_set
 
 
 if __name__ == '__main__':
